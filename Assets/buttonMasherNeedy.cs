9d081b1760c7ced3065542d80e410fc653b992a7
Joel Zaleschuk <joelzaleschuk@gmail.com>
2017-07-20 19:25:26 -0600

    Initial commit

---
+++ Assets/buttonMasherNeedy.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class buttonMasherNeedy : MonoBehaviour {
+    
+    public KMSelectable Solvebutton;
+    public TextMesh displayTxt;
+    int rngTxt = 0;
+    int isActive = 0;
+
+    protected int counter;
+
+    void Awake()
+    {
+        GetComponent<KMNeedyModule>().OnNeedyActivation += OnNeedyActivation;
+        GetComponent<KMNeedyModule>().OnNeedyDeactivation += OnNeedyDeactivation;
+        Solvebutton.OnInteract += Solve;
+        GetComponent<KMNeedyModule>().OnTimerExpired += OnTimerExpired;
+    }
+
+    protected bool Solve()
+    {
+        if (isActive == 1)
+        {
+            if (counter > -1)
+            {
+                counter--;
+                displayTxt.text = counter.ToString();
+                if (counter == -1)
+                {
+                    GetComponent<KMNeedyModule>().OnStrike();
+                    displayTxt.text = "__";
+                    GetComponent<KMNeedyModule>().OnPass();
+                    isActive = 0;
+                    return false;
+                }
+                return false;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    protected void OnNeedyActivation()
+    {
+        isActive = 1;
+        System.Random rnd = new System.Random();
+        rngTxt = rnd.Next(25, 46);
+        counter = rngTxt;
+        displayTxt.text = counter.ToString();
+    }
+
+    protected void OnNeedyDeactivation()
+    {
+        rngTxt = 0;
+        counter = rngTxt;
+        displayTxt.text = counter.ToString();
+        displayTxt.text = "__";
+        isActive = 0;
+    }
+
+    protected void OnTimerExpired()
+    {
+        if (counter == 0)
+        {
+            GetComponent<KMNeedyModule>().OnPass();
+            displayTxt.text = "__";
+        }
+        else
+        {
+            GetComponent<KMNeedyModule>().OnStrike();
+            displayTxt.text = "__";
+        }
+        isActive = 0;
+    }
+}
